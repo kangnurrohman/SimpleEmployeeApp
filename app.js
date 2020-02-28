60c627f5e6db50278a292d1f8d8c34fb3fd4db0c
Ainur Rohman <ainurrohman869@gmail.com>
2020-02-28 15:20:01 +0700

    Creating homepage & partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+const employeeRoutes = require('./routes/employees');
 
 dotenv.config({
  path: './config.env'
@@ -21,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(employeeRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
