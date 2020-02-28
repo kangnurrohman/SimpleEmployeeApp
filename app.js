bda9128f4372c8f4c53e067608dca8e25ed5ea8c
Ainur Rohman <ainurrohman869@gmail.com>
2020-02-28 16:22:02 +0700

    Creating & saving employee in database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const employeeRoutes = require('./routes/employees');
 
@@ -18,6 +19,9 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
  useCreateIndex: true
 });
 
+app.use(bodyParser.urlencoded({
+ extended: true
+}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
--- routes/employees.js
+++ routes/employees.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+const Employee = require('../models/employee');
+
 router.get('/', (req, res) => {
- res.render('index');
+	Employee.find({})
+		.then(employees => {
+			res.render('index', {
+				employees
+			});
+		})
+		.catch(err => {
+			console.log(err);
+
+		})
 });
 
+router.get('/employee/new', (req, res) => {
+	res.render('new');
+});
+
+router.post('/employee/new', (req, res) => {
+	let newEmployee = {
+		name: req.body.name,
+		designation: req.body.designation,
+		salary: req.body.salary
+	};
+
+	Employee.create(newEmployee)
+		.then(employee => {
+			res.redirect('/');
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
 module.exports = router;
